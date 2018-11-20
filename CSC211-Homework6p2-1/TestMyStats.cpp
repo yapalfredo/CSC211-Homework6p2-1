afa0ddbd6559020b267d27cf434ebf1649fc8b7a
yapalfredo <36459847+yapalfredo@users.noreply.github.com>
2018-11-19 21:38:03 -0500

    done (with seekp)

--- CSC211-Homework6p2-1/TestMyStats.cpp
+++ CSC211-Homework6p2-1/TestMyStats.cpp
@@ -1,35 +1,52 @@
 #include "MyStats.h"
-#include <string>
 
-const string s1 = "The initialized values of elements in the array are: ";
-const string s2 = "\nThe average of the values stored in the array is: ";
-const string s3 = "\nThe largest value stored in the array is: ";
-const string s4 = "\nThe smallest value stored in the array is: ";
+const char * c1 = "The initialized values of elements in the array are: \n\n";
+const char * c2 = "\nThe average of the values stored in the array is: ";
+const char * c3 = "\nThe largest value stored in the array is: ";
+const char * c4 = "\nThe smallest value stored in the array is: ";
 
 int main()
 {
 	MyStats stat;
 	ofstream outFile;
+	long pos;
 
-	outFile.open("Homework-6p2-1.txt");
+	outFile.open("Homework-6p2-1.txt");	// opens an ofstream object
 
-	/// this class will be loaded in the main function
-	cout << s1 << endl << endl;
-	outFile << s1 << endl << endl;
+	cout << c1;
+	//-------------------------
+	outFile.write(c1, 56);
 	stat.print(outFile);
+	//-------------------------
 	stat.print();
 
-	cout << s2 << stat.average() << endl;
-	outFile << s2 << stat.average() << endl;
+	cout << c2 << stat.average() << endl;
+	//-------------------------
+	pos = outFile.tellp();
+	outFile.seekp(pos);
+	outFile.write(c2, 51);
+	pos = outFile.tellp();
+	outFile.seekp(pos);
+	//-------------------------
+	outFile << stat.average() << endl;
 
-	cout << s3 << stat.findLargest() << endl;
-	outFile << s3 << stat.findLargest() << endl;
+	cout << c3 << stat.findLargest() << endl;
+	//-------------------------
+	pos = outFile.tellp();
+	outFile.seekp(pos);
+	outFile.write(c3, 43);
+	//-------------------------
+	outFile << stat.findLargest() << endl;
 
-	cout << s4 << stat.findSmallest() << endl << endl;
-	outFile << s4 << stat.findSmallest() << endl << endl;
+	cout << c4 << stat.findSmallest() << endl << endl;
+	//-------------------------
+	pos = outFile.tellp();
+	outFile.seekp(pos);
+	outFile.write(c4, 44);
+	//-------------------------
+	outFile << stat.findSmallest() << endl << endl;
 
-	outFile.close();
+	outFile.close();					// closes an ofstream object
 	system("pause");
 	return 0;
-
 }
